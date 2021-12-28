99073c243d27ba1fe43b87f2467b07d691cf25da
jamehoangnd21 <jameshoangnd21@gmail.com>
2021-12-28 22:38:00 +0700

    ok

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `SV Soccer`,
+    // author: {
+    //   name: `Kyle Mathews`,
+    //   summary: `who lives and works in San Francisco building useful things.`,
+    // },
+    // description: `A starter blog demonstrating what Gatsby can do.`,
+    // siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    // social: {
+    //   twitter: `kylemathews`,
+    // },
   },
   plugins: [
     `gatsby-plugin-image`,
